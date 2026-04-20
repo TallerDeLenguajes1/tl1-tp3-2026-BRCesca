f0ed6464957419c2a12a4550d4605c90076ef6b2
BrunoCesca <bruno.r.cesca@gmail.com>
2026-04-20 17:47:47 -0300

    implementada busqueda por ID en rama busca-nombre

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -10,17 +10,38 @@ void mostrarPersonas(char *vector[],int tamanio){
     }
 }
 
+void buscarNombre(char *vector[],int id){// tomo como id del primer nombre 1. id=1 entra a la componente v[0]
+    if(id>=1 && id<=5){
+        printf("El nombre con el id=%d es: %s",id , *(vector + (id -1)));
+    }else{
+        printf("No se encontro el valor buscado");
+    }
+}
+
+
+
 int main(){
     char *V[5];
     char *buff;
+    int id_buscar;
     buff=(char *)malloc(100*sizeof(char));
     for(int i=0;i<5;i++){
-        printf("\n\ningrese el %d nombre:", i+1);
+        printf("\ningrese el %d nombre:", i+1);
         gets(buff);
         *(V + i)=(char *)malloc((strlen(buff)+1)*sizeof(char));
         strcpy(*(V +i), buff);
     }
     mostrarPersonas(V,5);
+    printf("\n\n Ingrese el id del nombre que desea buscar:");
+    scanf("%d", &id_buscar);
+    buscarNombre(V,id_buscar);
+
+
+
+
+
+
+
 
 
     free(buff);
