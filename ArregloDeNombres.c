f7b49ea9bec4d867ded0a4a8ae24022aeb06bf21
BrunoCesca <bruno.r.cesca@gmail.com>
2026-04-20 18:43:15 -0300

    implementada busqueda por palabra clave

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -9,10 +9,21 @@ void mostrarPersonas(char *vector[],int tamanio){
         printf("\n\n%d)%s",(i+1),*(vector+i));
     }
 }
+int buscarNombre(char *vector[],char *pal){// con esta funcion consigo el indice de la palabra la cual imprimire en el main
+    for (int i = 0; i < 5; i++)
+    {
+        if(strstr(*(vector+i),pal)){
+            return i;
+        }
+    }
+    return -1;
+}
+
 
 int main(){
     char *V[5];
     char *buff;
+    int id_del_nombre;
     buff=(char *)malloc(100*sizeof(char));
     for(int i=0;i<5;i++){
         printf("\n\ningrese el %d nombre:", i+1);
@@ -21,6 +32,14 @@ int main(){
         strcpy(*(V +i), buff);
     }
     mostrarPersonas(V,5);
+    printf("\nIngrese la palabra clave para buscar:");
+    gets(buff);
+    id_del_nombre= buscarNombre(V,buff);
+    if(id_del_nombre!=-1){
+        printf("\nPalabra clave ingresada: %s\nNombre obtenido:%s",buff, *(V+id_del_nombre));
+    }else{
+        printf("No se encontraron nombres relacionados con la palabra clave ingresada");
+    }
 
 
     free(buff);
